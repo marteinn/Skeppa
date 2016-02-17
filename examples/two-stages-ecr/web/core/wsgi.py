0d0d486c1c1c0943a2028a5a8cb23321bd6e51b2
Martin Sandström <martin@marteinn.se>
2016-02-17 18:49:20 +0100

    Change django image name to web

---
+++ examples/two-stages-ecr/web/core/wsgi.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "core.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
